bcc1e57fa879573bd524035bd10bed8f3ff4efb6
farhan115 <106820039+farhan115@users.noreply.github.com>
2022-09-12 08:15:18 -0500

    Address Variable

---
+++ VariableAddress.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int var1;
+    cout<<"Enter var1 : ";
+    cin>>var1;
+    cout<<"Adress of the var1 :";
+    cout<<&var1;
+
+    return 0;
+}
